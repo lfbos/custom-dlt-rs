cdd46e15d073be975e4c46659d71ebc6e31e9157
Luis Boscan <lboscannava@gmail.com>
2025-10-06 13:20:53 -0500

    Base data types

---
+++ lib/src/sha256.rs
@@ -0,0 +1,42 @@
+use crate::U256;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct Hash(U256);
+
+impl Hash {
+    // hash anything that can be serde Serialized via ciborium
+    pub fn hash<T: serde::Serialize>(data: &T) -> Self {
+        let mut serialized: Vec<u8> = vec![];
+        if let Err(e) = ciborium::into_writer(data, &mut serialized) {
+            panic!(
+                "Failed to serialize data: {:?}. \
+                 This should not happen",
+                e
+            );
+        }
+
+        let hash = sha256::digest(&serialized);
+        let hash_bytes = hex::decode(hash).unwrap();
+        let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
+
+        Hash(U256::from_big_endian(&hash_array))
+    }
+
+    // check if a hash matches a target
+    pub fn matches_target(&self, target: U256) -> bool {
+        self.0 <= target
+    }
+
+    // zero hash
+    pub fn zero() -> Self {
+        Hash(U256::zero())
+    }
+}
+
+impl fmt::Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:x}", self.0)
+    }
+}
