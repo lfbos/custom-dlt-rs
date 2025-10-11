877a484604104e176f9c6f0e6e572ab8492ec921
Luis Boscan <lboscannava@gmail.com>
2025-10-11 14:44:14 -0500

    Saveable types

--- lib/types/block.rs
+++ lib/types/block.rs
@@ -1,11 +1,12 @@
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
-use super::{Transaction, TransactionOutput, TransactionInput};
+use super::{Transaction, TransactionInput, TransactionOutput};
+use crate::U256;
 use crate::error::{BtcError, Result};
 use crate::sha256::Hash;
-use crate::util::MerkleRoot;
-use crate::U256;
+use crate::util::{MerkleRoot, Saveable};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Read, Result as IoResult, Write};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Block {
@@ -142,6 +143,18 @@ impl Block {
         let output_value: u64 = outputs.values().map(|output| output.value).sum();
 
         Ok(input_value - output_value)
+    }
+}
+
+impl Saveable for Block {
+    fn load<I: Read>(reader: I) -> IoResult<Self> {
+        ciborium::de::from_reader(reader)
+            .map_err(|_| IoError::new(IoErrorKind::InvalidData, "Failed to deserialize Block"))
+    }
+
+    fn save<O: Write>(&self, writer: O) -> IoResult<()> {
+        ciborium::ser::into_writer(self, writer)
+            .map_err(|_| IoError::new(IoErrorKind::InvalidData, "Failed to serialize Block"))
     }
 }
 
